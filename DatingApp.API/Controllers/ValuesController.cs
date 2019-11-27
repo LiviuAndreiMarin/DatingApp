d0b2107953c780b20213738ce08b8fbe2909d49c
LiviuAndreiMarin <marinliviuandrei@yahoo.com>
2019-11-27 22:20:35 +0200

    End section 4

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -20,7 +20,8 @@ namespace DatingApp.API.Controllers
         {
             _context = context;
         }
-        //Get api/values
+        
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
